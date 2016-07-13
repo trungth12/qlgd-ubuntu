80661f4f27633d18bfdcb1941029f499eb896cc9
trungth12 <trungth@hpu.edu.vn>
2016-07-13 10:39:30 +0700

    Check error path khoa

--- app/controllers/daotao/users_controller.rb
+++ app/controllers/daotao/users_controller.rb
@@ -1,6 +1,6 @@
 class Daotao::UsersController < ApplicationController
 	def index
-		@users = User.all.map {|u| {:id => u.id, :text => u.username}}
+		@users = User.includes(:group).all.map {|u| {:id => u.id, :text => u.username}}
 		render json: @users, :root => false
 	end
 end
