891318747daee065d6dd09e319a7e4a6b03dae04
trungth12 <trungth@hpu.edu.vn>
2018-01-12 14:45:22 +0700

    QLGD HK1 Backup

--- sitemap.rb
+++ sitemap.rb
@@ -3,10 +3,13 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://qlgd.hpu.edu.vn'
 SitemapGenerator::Sitemap.create do
+  lop=1..299
   add '/', :changefreq => 'daily', :priority => 0.9
   add '/tenants', :changefreq => 'weekly'
-  add '/lop', :changefreq => 'weekly'
-  add '/sinh_viens', :changefreq => 'weekly'
-  add '/giang_viens', :changefreq => 'weekly'
-  add '/lich', :changefreq => 'weekly'
+  while i1 < 299 do
+    add '/lop/$i1', :changefreq => 'weekly'
+    add '/tenants/1/lop/' !! i1.tostring, :changefreq => 'weekly'
+    i1+=1
+  end
 end
+
